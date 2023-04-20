f4b9bc07e3d3a7267e71071b6c5435aacb60eeb3
Luis <roberto_lugo95@yahoo.com>
2023-04-20 00:09:34 -0500

    added search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 import Header from "./compnents/Header";
+import AnimeCard from "./compnents/AnimeCard";
+import SearchBar from "./compnents/SearchBar";
 
 function App() {
+  const [animeList, setAnimeList] = useState(null);
+
+  const getList = async () => {
+    const response = await axios.get(
+      `https://api.jikan.moe/v4/anime?q=naruto&limit=10`
+    );
+    // console.log(response.data);
+    setAnimeList(response.data);
+  };
+
+  useEffect(() => {
+    getList();
+  }, []);
+
   return (
     <div className="App">
       <Header />
+      <SearchBar setAnimeList={setAnimeList} />
     </div>
   );
 }
--- src/compnents/SearchBar.js
+++ src/compnents/SearchBar.js
@@ -1,5 +1,29 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-export default function SearchBar() {
-  return <></>;
-}
+const SearchBar = ({ setAnimeList }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+
+    const response = await axios.get(
+      `https://api.jikan.moe/v4/anime?q=${searchTerm}&limit=10`
+    );
+    setAnimeList(response.data.data);
+  };
+
+  return (
+    <form onSubmit={handleSearch}>
+      <input
+        type="text"
+        placeholder="Search for an anime"
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.target.value)}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default SearchBar;
